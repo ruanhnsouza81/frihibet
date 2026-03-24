70eed6956c1be11d0533e872623d384ead1e5dae
ruanhnsouza81 <ruanhnsouza81@gmail.com>
2026-03-24 14:42:39 -0300

    Add files via upload

---
+++ default.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">limegreen-cat-924273.hostingersite.com
+<title>Frigibet - Cassino Demo</title>
+
+<style>
+*{margin:0;padding:0;box-sizing:border-box;font-family:Arial}
+body{background:#12001e;color:#fff;padding-bottom:70px;padding-top:70px}
+
+/* HEADER */
+.header{
+background:linear-gradient(90deg,#5b0a8d,#2d004a);
+padding:12px;
+display:flex;
+justify-content:space-between;
+align-items:center;
+position:fixed;
+top:0;
+width:100%;https://limegreen-cat-924273.hostingersite.com/?LSCWP_CTRL=before_optm&nocache=1774220554550
+z-index:1000;https://limegreen-cat-924273.hostingersite.com/?LSCWP_CTRL=before_optm&nocache=1774220554550
+}4#K@eqB~cz
+.logo img{height:30px}
+.header div{display:flex;gap:10px;align-items:center}
+
+.saldo{
+background:#24003a;
+padding:6px 12px;
+border-radius:20px;
+font-weight:bold;
+}
+
+.depositar{
+background:#19ff7a;
+color:#000;
+padding:6px 14px;
+border-radius:20px;
+font-weight:bold;
+cursor:pointer;
+}
+
+/* SEÇÕES */
+.section{padding:15px;display:none}
+.grid{display:grid;grid-template-columns:repeat(3,1fr);gap:12px}
+
+/* CARD */
+.card{
+position:relative;
+background:#2b0044;
+border-radius:14px;
